35a6ebc819598ae5d63136bdf4f17cc3b207993d
Cerra1024 <sangarihar@gmail.com>
2026-05-07 03:05:42 -0400

    edited styling and updated readme

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import AppProviders from './context/AppProviders';
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
